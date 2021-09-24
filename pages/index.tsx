99c55ef74c7cdd2b1e6c0637699ecb33d8632f81
MikevPeeren <MikevPeeren@hotmail.com>
2021-09-24 15:55:32 +0200

    :sparkles: changing favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Time Tracking Dashboard</title>
         <meta name="description" content="Time Tracking Dashboard" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-32x32.png" />
       </Head>
 
       <main>
